bd2acf3ee096f8cac34727bf285210cc23246093
patrick-werner <pa.f.werner@gmail.com>
2024-01-24 12:03:42 +0100

    started creation of Patient Object

--- src/main/java/ClientTutorial.java
+++ src/main/java/ClientTutorial.java
@@ -1,4 +1,7 @@
 import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.r4.model.DateType;
+import org.hl7.fhir.r4.model.HumanName;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
 
 public class ClientTutorial {
@@ -9,6 +12,17 @@ public class ClientTutorial {
   }
 
   private static Patient getIsikPatient(FhirContext ctx) {
+    Patient pat = new Patient();
+    HumanName name = pat.addName();
+    name.addGiven("Patrick").addGiven("Fritz");
+    name.setFamily("Werner");
+    pat.setBirthDateElement(new DateType("1982-04-03"));
+
+    Identifier identifier = pat.addIdentifier();
+    identifier.getType().addCoding().setSystem("http://terminology.hl7.org/CodeSystem/v2-0203")
+        .setCode("MR");
+    identifier.setSystem("http://gefyra.de/sid/Patientennummer").setValue("0815");
+    return pat;
   }
 
 }
