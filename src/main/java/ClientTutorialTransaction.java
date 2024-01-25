cbdd4859a6d644f03aab501760b15b603147307f
patrick-werner <pa.f.werner@gmail.com>
2024-01-25 10:29:54 +0100

    added transaction example

--- src/main/java/ClientTutorialTransaction.java
+++ src/main/java/ClientTutorialTransaction.java
@@ -1,15 +1,17 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
-import ca.uhn.fhir.rest.api.CacheControlDirective;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
-import org.hl7.fhir.instance.model.api.IBaseOperationOutcome;
 import org.hl7.fhir.r4.model.Address;
 import org.hl7.fhir.r4.model.Address.AddressType;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryRequestComponent;
+import org.hl7.fhir.r4.model.Bundle.BundleType;
+import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
 import org.hl7.fhir.r4.model.Condition;
 import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.Encounter;
@@ -18,13 +20,14 @@ import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
 import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.HumanName.NameUse;
+import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.StringType;
 
-public class ClientTutorial {
+public class ClientTutorialTransaction {
 
   public static void main(String[] args) {
     FhirContext ctx = FhirContext.forR4Cached();
@@ -33,17 +36,44 @@ public class ClientTutorial {
     IGenericClient client = ctx.newRestfulGenericClient(serverBase);
     Patient pat = getIsikPatient(ctx);
     System.out.println(iParser.encodeResourceToString(pat));
-    //sendResourceToServer(client, pat);
-    List<Patient> patients = searchPatient(client);
-    updatePatient(client, patients.get(0));
-    //deletePatient(client, patients.get(0), iParser);
-    Condition condition = addCondition(client, "259");
-    sendResourceToServer(client, condition);
-    Encounter encounter = addEncounter(client, "259");
-    sendResourceToServer(client, encounter);
+    MethodOutcome outcome = validateResource(client, pat);
+    //System.out.println(iParser.encodeResourceToString(outcome.getOperationOutcome()));
+    pat.setId(IdType.newRandomUuid());
+
+    Encounter encounter = addEncounter(client, pat);
+    encounter.setId(IdType.newRandomUuid());
+    Condition condition = addCondition(client, pat, encounter);
+    condition.setId(IdType.newRandomUuid());
+    List<Resource> resources = new ArrayList<>();
+    resources.add(pat);
+    resources.add(encounter);
+    resources.add(condition);
+    //Bundle bundle = sendTransactionBundleToServer(client, resources);
+    //System.out.println(iParser.encodeResourceToString(bundle));
   }
 
-  private static Encounter addEncounter(IGenericClient client, String patientId) {
+  private static MethodOutcome validateResource(IGenericClient client, Patient pat) {
+    MethodOutcome outcome = client.validate().resource(pat).execute();
+    return outcome;
+  }
+
+  private static Bundle sendTransactionBundleToServer(IGenericClient client,
+      List<Resource> resources) {
+    Bundle bundle = new Bundle();
+    bundle.setType(BundleType.TRANSACTION);
+    resources.forEach(r -> {
+      BundleEntryComponent bundleEntryComponent = bundle.addEntry();
+      bundleEntryComponent.setResource(r);
+      bundleEntryComponent.setFullUrl(r.getId());
+      BundleEntryRequestComponent request = bundleEntryComponent.getRequest();
+      request.setMethod(HTTPVerb.POST);
+      request.setUrl(r.getResourceType().name());
+    });
+    Bundle responseBundle = client.transaction().withBundle(bundle).execute();
+    return responseBundle;
+  }
+
+  private static Encounter addEncounter(IGenericClient client, Patient patient) {
     Encounter encounter = new Encounter();
     Identifier identifier = encounter.addIdentifier();
     identifier.getType().addCoding().setSystem("http://terminology.hl7.org/CodeSystem/v2-0203")
@@ -54,51 +84,19 @@ public class ClientTutorial {
     encounter.setStatus(EncounterStatus.PLANNED);
     encounter.getClass_().setSystem("http://terminology.hl7.org/CodeSystem/v3-ActCode")
         .setCode("IMP").setDisplay("stationärer Aufenthalt");
-    encounter.setSubject(new Reference("Patient/"+ patientId));
+    encounter.setSubject(new Reference(patient.getId()));
     return encounter;
   }
 
-  private static Condition addCondition(IGenericClient client, String patientId) {
+  private static Condition addCondition(IGenericClient client, Patient patient,
+      Encounter encounter) {
     Condition condition = new Condition();
-    condition.setSubject(new Reference("Patient/" + patientId));
+    condition.setSubject(new Reference(patient.getId()));
     condition.getCode().addCoding().setCode("R05").setDisplay("Husten")
         .setSystem("http://fhir.de/CodeSystem/bfarm/icd-10-gm");
     condition.setRecordedDate(new Date());
+    condition.setEncounter(new Reference(encounter.getId()));
     return condition;
-
-  }
-
-  private static void deletePatient(IGenericClient client, Patient patient, IParser iParser) {
-    MethodOutcome outcome = client.delete()
-        .resource(patient).execute();
-    IBaseOperationOutcome operationOutcome = outcome.getOperationOutcome();
-    System.out.println(iParser.encodeResourceToString(operationOutcome));
-  }
-
-  private static void updatePatient(IGenericClient client, Patient patient) {
-    patient.addName().addGiven(UUID.randomUUID().toString());
-    MethodOutcome outcome = client.update()
-        .resource(patient).execute();
-    System.out.println(outcome.getId());
-  }
-
-  private static List<Patient> searchPatient(IGenericClient client) {
-//    Bundle bundle = client.search().forResource(Patient.class)
-//        .where(Patient.IDENTIFIER.exactly()
-//            .systemAndIdentifier("http://gefyra.de/sid/Patientennummer", "01234569"))
-//        .returnBundle(Bundle.class)
-//        .execute();
-    Bundle bundle = client.search()
-        .byUrl("Patient?identifier=http://gefyra.de/sid/Patientennummer|01234569")
-        .returnBundle(Bundle.class)
-        // ohne cache suchen (default 5 Minuten)
-        .cacheControl(new CacheControlDirective().setNoCache(true))
-        .execute();
-
-    System.out.println("Total: " + bundle.getTotal());
-    List<Patient> list = bundle.getEntry().stream().map(c -> c.getResource()).map(r -> (Patient) r)
-        .toList();
-    return list;
   }
 
   private static void sendResourceToServer(IGenericClient client, Resource res) {
