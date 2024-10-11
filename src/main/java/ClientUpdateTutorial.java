ba7521b16678913dcf471ee027643c2e915a17d2
Patrick Werner <pa.f.werner@gmail.com>
2024-10-11 11:07:04 +0200

    after search & update

---
+++ src/main/java/ClientUpdateTutorial.java
@@ -0,0 +1,114 @@
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import java.util.Date;
+import java.util.UUID;
+import org.hl7.fhir.r4.model.Address;
+import org.hl7.fhir.r4.model.Address.AddressType;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.DateType;
+import org.hl7.fhir.r4.model.Encounter;
+import org.hl7.fhir.r4.model.Encounter.EncounterStatus;
+import org.hl7.fhir.r4.model.Enumeration;
+import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
+import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r4.model.HumanName;
+import org.hl7.fhir.r4.model.HumanName.NameUse;
+import org.hl7.fhir.r4.model.Identifier;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.StringType;
+
+public class ClientUpdateTutorial {
+
+  private static FhirContext ctx;
+  private static IParser iParser;
+  private static IGenericClient client;
+  private static String sidSystem = "http://gefyra.de/fhir/sid/Patientennummer";
+  private static String sidNumber = "9849849846984964132197";
+
+  public static void main(String[] args) {
+    ctx = FhirContext.forR4Cached();
+    client = ctx.newRestfulGenericClient("https://fhir.echinos.eu/fhir");
+    iParser = ctx.newJsonParser();
+    iParser.setPrettyPrint(true);
+
+    Bundle patientBundle = client.search()
+        .byUrl("Patient?identifier=" + sidSystem + "|" + sidNumber
+            + "&_total=accurate&_count=100&_revinclude=Encounter:subject&_revinclude=Condition:subject")
+        .returnBundle(Bundle.class).execute();
+
+    System.out.println(iParser.encodeResourceToString(patientBundle));
+    System.out.println(patientBundle.getTotal());
+
+    Patient patient = (Patient) patientBundle.getEntry().get(0).getResource();
+    patient.getNameFirstRep().addGiven(UUID.randomUUID().toString());
+
+    MethodOutcome outcome = client.update().resource(patient).execute();
+    System.out.println(outcome.getId());
+  }
+
+  static Patient createIsikPatient() {
+    Patient patient = new Patient();
+    HumanName humanName = patient.addName();
+    humanName.setFamily("Werner").addGiven("Patrick").addGiven("Fritz")
+        .setUse(NameUse.OFFICIAL);
+    patient.setBirthDateElement(new DateType("1982-04-03"));
+    //alternativ:
+    //patient.setBirthDate(new Date());
+    patient.setGender(AdministrativeGender.OTHER);
+    Enumeration<AdministrativeGender> genderElement = patient.getGenderElement();
+    genderElement.addExtension().setUrl("http://fhir.de/StructureDefinition/gender-amtlich-de")
+        .setValue(new Coding("http://fhir.de/CodeSystem/gender-amtlich-de", "X", "unbestimmt"));
+    Identifier identifier = patient.addIdentifier();
+    identifier.getType().addCoding()
+        .setSystem("http://terminology.hl7.org/CodeSystem/v2-0203")
+        .setCode("MR")
+        .setDisplay("Patientennummer");
+    identifier.setSystem(sidSystem).setValue(sidNumber);
+    patient.setActive(true);
+    //Adresse
+    Address address = patient.addAddress();
+    address.setType(AddressType.BOTH);
+    StringType line = address.addLineElement();
+    line.setValue("Musterstraße 130");
+    Extension strassenName = line.addExtension();
+    strassenName.setUrl("http://hl7.org/fhir/StructureDefinition/iso21090-ADXP-streetName")
+        .setValue(new StringType("Musterstraße"));
+    Extension hausnummer = line.addExtension();
+    hausnummer.setUrl("http://hl7.org/fhir/StructureDefinition/iso21090-ADXP-houseNumber")
+        .setValue(new StringType("130"));
+    address.setCity("Mannheim");
+    address.setPostalCode("68169");
+    return patient;
+  }
+
+  static Condition getIsiKCondition(String patientId) {
+    Condition condition = new Condition();
+    condition.setSubject(new Reference(patientId));
+    condition.setRecordedDate(new Date());
+    condition.addNote().setText("Ich bin eine Notiz");
+    condition.getCode().addCoding().setSystem("http://fhir.de/CodeSystem/bfarm/icd-10-gm")
+        .setCode("R05").setDisplay("Husten");
+    return condition;
+  }
+
+  private static Encounter getIsiKEncounter(String patientId) {
+    Encounter encounter = new Encounter();
+    encounter.setSubject(new Reference(patientId));
+    Identifier identifier = encounter.addIdentifier();
+    identifier.getType().addCoding()
+        .setSystem("http://terminology.hl7.org/CodeSystem/v2-0203")
+        .setCode("VN")
+        .setDisplay("visit number");
+    identifier.setSystem("http://gefyra.de/fhir/sid/Aufnahmenummer");
+    identifier.setValue("515816941");
+    encounter.addType().addCoding().setSystem("http://fhir.de/CodeSystem/Kontaktebene")
+        .setCode("abteilungskontakt").setDisplay("Abteilungskontakt");
+    encounter.setStatus(EncounterStatus.PLANNED);
+    return encounter;
+  }
+}
