6cd362376f158b70fc1da3dff0aecd4180c2ac82
Patrick Werner <pa.f.werner@gmail.com>
2024-10-11 13:23:05 +0200

    beginning of Validation

--- src/main/java/ISiKValidator.java
+++ src/main/java/ISiKValidator.java
@@ -1,5 +1,7 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.validation.FhirValidator;
+import java.io.IOException;
+import org.hl7.fhir.common.hapi.validation.support.NpmPackageValidationSupport;
 
 public class ISiKValidator extends FhirValidator {
 
@@ -9,10 +11,12 @@ public class ISiKValidator extends FhirValidator {
    *
    * @param theFhirContext
    */
-  public ISiKValidator(FhirContext theFhirContext) {
+  public ISiKValidator(FhirContext theFhirContext) throws IOException {
     super(theFhirContext);
 
-
+    NpmPackageValidationSupport npmPackageValidationSupport = new NpmPackageValidationSupport(theFhirContext);
+    npmPackageValidationSupport.loadPackageFromClasspath("classpath:/packages/de.basisprofil.r4-1.5.0.tgz");
+    npmPackageValidationSupport.loadPackageFromClasspath("classpath:/packages/de.gematik.isik-basismodul-4.0.0.tgz");
 
   }
 }
