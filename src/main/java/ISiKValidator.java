18ea3d64a472544e1748aa0a4344866f39fea3e8
Patrick Werner <pa.f.werner@gmail.com>
2024-10-11 13:30:16 +0200

    beginning of Validation

--- src/main/java/ISiKValidator.java
+++ src/main/java/ISiKValidator.java
@@ -1,7 +1,14 @@
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.validation.FhirValidator;
 import java.io.IOException;
+import org.hl7.fhir.common.hapi.validation.support.CachingValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.CommonCodeSystemsTerminologyService;
+import org.hl7.fhir.common.hapi.validation.support.InMemoryTerminologyServerValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.NpmPackageValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.SnapshotGeneratingValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
+import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
 
 public class ISiKValidator extends FhirValidator {
 
@@ -18,5 +25,17 @@ public class ISiKValidator extends FhirValidator {
     npmPackageValidationSupport.loadPackageFromClasspath("classpath:/packages/de.basisprofil.r4-1.5.0.tgz");
     npmPackageValidationSupport.loadPackageFromClasspath("classpath:/packages/de.gematik.isik-basismodul-4.0.0.tgz");
 
+    ValidationSupportChain validationSupportChain = new ValidationSupportChain(
+        npmPackageValidationSupport,
+        new DefaultProfileValidationSupport(theFhirContext),
+        new CommonCodeSystemsTerminologyService(theFhirContext),
+        new InMemoryTerminologyServerValidationSupport(theFhirContext),
+        new SnapshotGeneratingValidationSupport(theFhirContext));
+    CachingValidationSupport cachingValidationSupport = new CachingValidationSupport(validationSupportChain);
+    FhirInstanceValidator validator = new FhirInstanceValidator(cachingValidationSupport);
+    registerValidatorModule(validator);
+
+    validator.setNoExtensibleWarnings(true);
+    validator.setErrorForUnknownProfiles(true);
   }
 }
